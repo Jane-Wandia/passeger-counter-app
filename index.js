bc8c070e23a7012dcb1f4c86d0f1d80b4b86fd2a
Jane-Wandia <janekigenyi2018@gmail.com>
2022-04-12 16:06:56 +0300

    added a button, and a funtion as well as styled in css

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-document.getelementid("count-el".innerText=5)
+//document.getelementid("count-el".innerText=5)
+
+//let count = 5
+
+//count = 3
+//count= 1
+
+//console.log(count)
+
+function increment() {
+    console.log("The button was clicked")
+}
